651bd78383a76e91d8b74367c02cfa43a824f813
JulianParra01 <julianparra219@gmail.com>
2023-07-20 11:37:45 -0500

    Insercion del apartado Login y Logout

---
+++ src/conf_auth.js
@@ -0,0 +1,8 @@
+
+const conf_auth = {
+    domain: 'dev-todosalau.us.auth0.com',
+    clientId: 'ugXZyMUCckJfASGDNXS3fiog2XL86AU5',
+    redirectUri: window.location.origin
+}
+
+export default conf_auth
